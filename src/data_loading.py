408e828b8c48e376f0f0c78ac6f9204bd8f7bb42
asuther <alexsutherland2121@gmail.com>
2020-01-22 08:32:52 -0800

    Added loading of sub-challenge 2 and 3 data

--- src/data_loading.py
+++ src/data_loading.py
@@ -10,3 +10,24 @@ def load_sc1_data():
         .set_index('PATIENTID')
 
     return sc1_Phase1_GE_FeatureMatrix, sc1_Phase1_GE_Outcome['SURVIVAL_STATUS'], sc1_Phase1_GE_Phenotype
+
+def load_sc2_data():
+    sc2_Phase1_CN_FeatureMatrix = pd.read_csv(f'{constants.base_dir}/data/raw/sc2_Phase1_CN_FeatureMatrix.tsv', sep    ='\t')\
+        .set_index('PATIENTID')
+    sc2_Phase1_CN_Outcome = pd.read_csv(f'{constants.base_dir}/data/raw/sc2_Phase1_CN_Outcome.tsv', sep='\t')\
+        .set_index('PATIENTID')
+    sc2_Phase1_CN_Phenotype = pd.read_csv(f'{constants.base_dir}/data/raw/sc2_Phase1_CN_Phenotype.tsv', sep='\t')\
+        .set_index('PATIENTID')
+    
+    return sc2_Phase1_CN_FeatureMatrix, sc2_Phase1_CN_Outcome['SURVIVAL_STATUS'], sc2_Phase1_CN_Phenotype
+
+
+def load_sc3_data():
+    sc3_Phase1_CN_GE_FeatureMatrix = pd.read_csv(f'{constants.base_dir}/data/raw/sc3_Phase1_CN_GE_FeatureMatrix.tsv',sep='\t')\
+        .set_index('PATIENTID')
+    sc3_Phase1_CN_GE_Outcome = pd.read_csv(f'{constants.base_dir}/data/raw/sc3_Phase1_CN_GE_Outcome.tsv', sep='\t') \
+        .set_index('PATIENTID')
+    sc3_Phase1_CN_GE_Phenotype = pd.read_csv(f'{constants.base_dir}/data/raw/sc3_Phase1_CN_GE_Phenotype.tsv', sep='\t') \
+        .set_index('PATIENTID')
+
+    return sc3_Phase1_CN_GE_FeatureMatrix, sc3_Phase1_CN_GE_Outcome['SURVIVAL_STATUS'], sc3_Phase1_CN_GE_Phenotype
